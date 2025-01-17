188504060330e91d17a3d9c9af0c8e30364f7a58
Kirtan Modi <modikirtan19@gmail.com>
2025-01-18 01:04:47 +0530

    base template

--- src/pages/dashboards/demo1/light-sidebar/Demo1LightSidebarPage.tsx
+++ src/pages/dashboards/demo1/light-sidebar/Demo1LightSidebarPage.tsx
@@ -1,15 +1,9 @@
-import { Fragment, useState } from 'react';
 import { Container } from '@/components/container';
-import { Toolbar, ToolbarActions, ToolbarHeading } from '@/layouts/demo1/toolbar';
-import { Demo1LightSidebarContent } from './';
+import { Toolbar, ToolbarHeading } from '@/layouts/demo1/toolbar';
+import { Fragment, useState } from 'react';
 
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { Calendar } from '@/components/ui/calendar';
+import { addDays } from 'date-fns';
 import { DateRange } from 'react-day-picker';
-import { addDays, format } from 'date-fns';
-import { cn } from '@/lib/utils';
-import { KeenIcon } from '@/components/keenicons';
-import Counter from '@/reducer/counter/Counter';
 
 const Demo1LightSidebarPage = () => {
   const [date, setDate] = useState<DateRange | undefined>({
@@ -22,7 +16,7 @@ const Demo1LightSidebarPage = () => {
       <Container>
         <Toolbar>
           <ToolbarHeading title="Dashboard" description="Welcome to the Dashboard" />
-          <Counter />
+          {/* <Counter /> */}
           {/* <ToolbarActions>
             <Popover>
               <PopoverTrigger asChild>
